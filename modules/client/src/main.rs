8fc2a19d9aed3f6f9cc1457b297451cf34bdc976
psachmann <20001160+psachmann@users.noreply.github.com>
2024-08-10 17:35:26 +0200

    formatting the code

--- modules/client/src/main.rs
+++ modules/client/src/main.rs
@@ -3,9 +3,11 @@ use leptos::*;
 fn main() {
     _ = console_error_panic_hook::set_once();
 
-    mount_to_body(|| view! {
-        <div class="flex justify-center">
-            <p class="text-4xl font-bold pt-16">{ "Hello, world from Leptos!" }</p>
-        </div>
+    mount_to_body(|| {
+        view! {
+            <div class="flex justify-center">
+                <p class="text-4xl font-bold pt-16">{ "Hello, world from Leptos!" }</p>
+            </div>
+        }
     });
 }
