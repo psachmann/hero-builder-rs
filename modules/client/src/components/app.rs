f614041e4a1570af6c55467130cccd711f4aa62c
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-08-15 08:43:32 +0200

    working on site layout

--- modules/client/src/components/app.rs
+++ modules/client/src/components/app.rs
@@ -1,31 +1,42 @@
 use leptos::*;
+use leptos_router::*;
+
+use super::layout::Layout;
 
 #[component]
-pub fn app() -> impl IntoView {
+fn counter() -> impl IntoView {
     let counter = create_rw_signal(0);
 
     view! {
         <div class="container mx-auto">
-            <div class="flex flex-col">
-                <div class="justify-center">
-                    <p class="text-4xl font-bold pt-16">{ "Hello, world from Leptos!" }</p>
+            <div class="flex flex-row gap-4">
+                <div>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={ move |_| counter.set(counter.get() - 1) }>
+                        { "Decrement" }
+                    </button>
                 </div>
-                <div class="grid grid-cols-3 grid-flow-col gap-4">
-                    <div class="grid-cols-1">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={ move |_| counter.set(counter.get() - 1) }>
-                            { "Decrement" }
-                        </button>
-                    </div>
-                    <div class="grid-cols-1">
-                        <p class="text-lg font-bold pt-16">{ counter }</p>
-                    </div>
-                    <div class="grid-cols-1">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={ move |_| counter.set(counter.get() + 1) }>
-                            { "Increment" }
-                        </button>
-                    </div>
+                <div>
+                    <p class="text-lg font-bold pt-16">{ counter }</p>
+                </div>
+                <div>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={ move |_| counter.set(counter.get() + 1) }>
+                        { "Increment" }
+                    </button>
                 </div>
             </div>
         </div>
+    }
+}
+
+#[component]
+pub fn app() -> impl IntoView {
+    view! {
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path="/" view=Counter />
+                </Routes>
+            </Layout>
+        </Router>
     }
 }
--- modules/client/src/components/layout/mod.rs
+++ modules/client/src/components/layout/mod.rs
@@ -7,14 +7,21 @@ use leptos::*;
 #[component]
 pub fn layout(children: ChildrenFn) -> impl IntoView {
     view! {
-        <div>
-            <header />
-            <main>
+        <div class="grid grid-cols-10 grid-rows-3 gap-2 bg-slate-400">
+            <header class="col-span-10 row-span-1 bg-slate-200">
+                <h1>{"Header"}</h1>
+            </header>
+            <aside class="col-span-2 row-span-1 bg-slate-200">
+                <h1>{"Sidebar"}</h1>
+            </aside>
+            <main class="col-span-8 row-span-1 bg-slate-200">
                 <div>
                     {children()}
                 </div>
             </main>
-            <footer />
+            <footer class="col-span-10 row-span-1 bg-slate-200">
+                <h1>{"Footer"}</h1>
+            </footer>
         </div>
     }
 }
