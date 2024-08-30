ccffc756c63b2876143db86ec8d7f58d02b4d05f
psachmann <20001160+psachmann@users.noreply.github.com>
2024-08-30 20:35:07 +0200

    adding customized tailwind css

--- modules/client/tailwind.config.js
+++ modules/client/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,rs}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Menlo', 'Monaco', 'Courier New', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
