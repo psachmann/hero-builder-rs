e60338bcde3329d33f011bb26b889869001e362d
psachmann <20001160+psachmann@users.noreply.github.com>
2024-09-05 21:07:51 +0200

    running formatter

--- modules/client/src/components/editor/attributes.rs
+++ modules/client/src/components/editor/attributes.rs
@@ -1,7 +1,9 @@
 use leptos::*;
 
 use crate::{
-    i18n::{t, use_i18n}, models::Hero, utils::{use_hero_with_id, use_param, ParamUuid}
+    i18n::{t, use_i18n},
+    models::Hero,
+    utils::{use_hero_with_id, use_param, ParamUuid},
 };
 
 use super::editor_params::EditorParams;
@@ -13,9 +15,19 @@ pub fn counter(count: RwSignal<i32>) -> impl IntoView {
 
     view! {
         <div class="inline-flex flex-row bg-blue-400 rounded-full justify-center items-center">
-            <p class="text-xl font-semibold px-2 hover:cursor-pointer" on:click=move |_| { decrement() }>{"-"}</p>
+            <p
+                class="text-xl font-semibold px-2 hover:cursor-pointer"
+                on:click=move |_| { decrement() }
+            >
+                {"-"}
+            </p>
             <p class="text-center font-semibold bg-slate-200 rounded-full w-6 h-6">{count}</p>
-            <p class="text-xl font-semibold px-2 hover:cursor-pointer" on:click=move |_| { increment() }>{"+"}</p>
+            <p
+                class="text-xl font-semibold px-2 hover:cursor-pointer"
+                on:click=move |_| { increment() }
+            >
+                {"+"}
+            </p>
         </div>
     }
 }
@@ -30,52 +42,56 @@ pub fn attributes() -> impl IntoView {
         <div class="container mx-auto flex flex-col gap-2 bg-slate-100 rounded-lg shadow-md p-4">
             <p class="text-black text-xl font-semibold">{t!(i18n, editor.attributes.title)}</p>
             <div class="border-2 rounded-lg">
-            <table class="table-auto w-full">
-                <thead>
-                    <tr class="border-b-2">
-                        <th class="text-start">{{t!(i18n, editor.attributes.title)}}</th>
-                        <th class="text-center">{{t!(i18n, editor.attributes.value)}}</th>
-                        <th class="text-center">{{t!(i18n, editor.attributes.trained)}}</th>
-                        <th class="text-center">{{t!(i18n, editor.attributes.initial)}}</th>
-                        <th class="text-center">{{t!(i18n, editor.attributes.modifier)}}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        hero.get().attributes.into_iter().map(|(name, value)| {
-                            let current = move || value.0.get() + value.1.get() + value.2.get();
-
-                            view! {
-                                <tr class="border-b-2 last:border-0 hover:bg-gray-200">
-                                    <td class="text-start">{name}</td>
-                                    <td class="text-center">
-                                        <p class="font-semibold">{current}</p>
-                                    </td>
-                                    <td class="text-center">
-                                        <Counter count={value.0.clone()} />
-                                    </td>
-                                    <td class="text-center">
-                                        <Counter count={value.1.clone()} />
-                                    </td>
-                                    <td class="text-center">
-                                        <Counter count={value.2.clone()} />
-                                    </td>
-                                </tr>
-                            }
-                        }).collect::<Vec<_>>()
-                    }
-                </tbody>
-            </table>
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr class="border-b-2">
+                            <th class="text-start">{{ t!(i18n, editor.attributes.title) }}</th>
+                            <th class="text-center">{{ t!(i18n, editor.attributes.value) }}</th>
+                            <th class="text-center">{{ t!(i18n, editor.attributes.trained) }}</th>
+                            <th class="text-center">{{ t!(i18n, editor.attributes.initial) }}</th>
+                            <th class="text-center">{{ t!(i18n, editor.attributes.modifier) }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {hero
+                            .get()
+                            .attributes
+                            .into_iter()
+                            .map(|(name, value)| {
+                                let current = move || value.0.get() + value.1.get() + value.2.get();
+                                view! {
+                                    <tr class="border-b-2 last:border-0 hover:bg-gray-200">
+                                        <td class="text-start">{name}</td>
+                                        <td class="text-center">
+                                            <p class="font-semibold">{current}</p>
+                                        </td>
+                                        <td class="text-center">
+                                            <Counter count=value.0.clone() />
+                                        </td>
+                                        <td class="text-center">
+                                            <Counter count=value.1.clone() />
+                                        </td>
+                                        <td class="text-center">
+                                            <Counter count=value.2.clone() />
+                                        </td>
+                                    </tr>
+                                }
+                            })
+                            .collect::<Vec<_>>()}
+                    </tbody>
+                </table>
             </div>
-            <p class="text-black text-xl font-semibold pt-4">{t!(i18n, editor.derived_attributes.title)}</p>
+            <p class="text-black text-xl font-semibold pt-4">
+                {t!(i18n, editor.derived_attributes.title)}
+            </p>
             <table class="table-auto border-2 rounded-lg w-full">
-            <thead>
+                <thead>
                     <tr>
-                        <th class="text-start">{{t!(i18n, editor.attributes.title)}}</th>
-                        <th class="text-start">{{t!(i18n, editor.attributes.value)}}</th>
-                        <th class="text-start">{{t!(i18n, editor.attributes.trained)}}</th>
-                        <th class="text-start">{{t!(i18n, editor.attributes.initial)}}</th>
-                        <th class="text-start">{{t!(i18n, editor.attributes.modifier)}}</th>
+                        <th class="text-start">{{ t!(i18n, editor.attributes.title) }}</th>
+                        <th class="text-start">{{ t!(i18n, editor.attributes.value) }}</th>
+                        <th class="text-start">{{ t!(i18n, editor.attributes.trained) }}</th>
+                        <th class="text-start">{{ t!(i18n, editor.attributes.initial) }}</th>
+                        <th class="text-start">{{ t!(i18n, editor.attributes.modifier) }}</th>
                     </tr>
                 </thead>
                 <tbody>
